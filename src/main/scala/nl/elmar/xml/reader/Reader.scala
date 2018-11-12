707e58a97d4ba149786d49acfb39f8278a65734d
Igor Mielientiev <igor.melent@gmail.com>
2018-11-12 14:25:19 +0100

    Add `withDefault` combinator

--- src/main/scala/nl/elmar/xml/reader/Reader.scala
+++ src/main/scala/nl/elmar/xml/reader/Reader.scala
@@ -7,8 +7,8 @@ import scala.xml.NodeSeq
 object Reader {
   def apply[A](f: NodeSeq => Result[A]): Reader[A] = Kleisli(f)
 
-  def pure[A](a: A) = Reader(_ => valid(a))
+  def pure[A](a: A): Reader[A] = Reader(_ => valid(a))
 
-  def reader[A: Reader] = implicitly[Reader[A]]
+  def reader[A: Reader]: Reader[A] = implicitly[Reader[A]]
 
 }
